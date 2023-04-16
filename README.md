05ac0929efcfd7835de39c871989cafa5329af7b
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-04-16 11:52:23 +0700

    Init repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# Documentation for AnyLabeling
+
+<p align="center">
+  <img alt="AnyLabeling" style="width: 128px; max-width: 100%; height: auto;" src="https://user-images.githubusercontent.com/18329471/232250539-2b15b9ee-5593-41d0-ba22-e0442f314cce.png"/>
+  <h1 align="center">🌟 AnyLabeling 🌟</h1>
+  <p align="center">Effortless data labeling with AI support from <b>YOLO</b> and <b>Segment Anything</b>!<p>
+  <p align="center"><b>AnyLabeling = LabelImg + Labelme + Improved UI + Auto-labeling</b><p>
+</p>
+
+![](https://i.imgur.com/waxVImv.png)
+
+[![PyPI](https://img.shields.io/pypi/v/anylabeling)](https://pypi.org/project/anylabeling)
+[![license](https://img.shields.io/github/license/vietanhdev/anylabeling.svg)](https://github.com/vietanhdev/anylabeling/blob/master/LICENSE)
+[![open issues](https://isitmaintained.com/badge/open/vietanhdev/anylabeling.svg)](https://github.com/vietanhdev/anylabeling/issues)
+[![Pypi Downloads](https://pepy.tech/badge/anylabeling)](https://pypi.org/project/anylabeling/)
+
+<a href="https://www.youtube.com/watch?v=5iQSGL7ebXE">
+  <img alt="AnyLabeling" src="https://user-images.githubusercontent.com/18329471/231320488-2f8133bc-6b51-48f8-82a6-dd3b267f5156.png"/>
+</a>
+
+[https://github.com/vietanhdev/anylabeling](https://github.com/vietanhdev/anylabeling)
---
+++ theme.config.tsx
@@ -0,0 +1,140 @@
+import type { DocsThemeConfig } from "nextra-theme-docs";
+import { useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+
+const logo = (
+  <>
+    <div className="flex flex-row align-middle justify-center">
+      <img className="h-12 w-auto rounded-md" src="/logo.png"></img>
+      <h1 className="text-2xl ml-2 font-bold inline-block" style={{lineHeight: "3rem"}}>AnyLabeling</h1>
+    </div>
+    <style jsx>{`
+      span {
+        padding: 0.5rem 0.5rem 0.5rem 0;
+        mask-image: linear-gradient(
+          60deg,
+          black 25%,
+          rgba(0, 0, 0, 0.2) 50%,
+          black 75%
+        );
+        mask-size: 400%;
+        mask-position: 0%;
+      }
+      span:hover {
+        mask-position: 100%;
+        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+      }
+    `}</style>
+  </>
+);
+
+const config: DocsThemeConfig = {
+  project: {
+    link: "https://github.com/vietanhdev/anylabeling",
+  },
+  docsRepositoryBase: "https://github.com/vietanhdev/anylabeling-docs",
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – AnyLabeling",
+      };
+    }
+  },
+  logo,
+  head: function useHead() {
+    const { title } = useConfig();
+    const { route } = useRouter();
+    const socialCard =
+      route === "/" || !title
+        ? "https://anylabeling.com/og.jpeg"
+        : `https://anylabeling.com/api/og?title=${title}`;
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Effortless AI-assisted data labeling with AI support from Segment Anything and YOLO!"
+        />
+        <meta
+          name="og:description"
+          content="Effortless AI-assisted data labeling with AI support from Segment Anything and YOLO!"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="anylabeling.com" />
+        <meta name="twitter:url" content="https://anylabeling.com" />
+        <meta
+          name="og:title"
+          content={title ? title + " – AnyLabeling" : "AnyLabeling"}
+        />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="AnyLabeling" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon-dark.png"
+          type="image/png"
+          media="(prefers-color-scheme: light)"
+        />
+      </>
+    );
+  },
+  banner: {
+    key: "anylabeling-0.3.0.1",
+    text: (
+      <a href="https://github.com/vietanhdev/anylabeling" target="_blank" rel="noreferrer">
+        🎉 Check out the latest AnyLabeling for AI-assisted image labeling! 🎉
+      </a>
+    ),
+  },
+  editLink: {
+    text: "Edit this page on GitHub →",
+  },
+  feedback: {
+    content: "Question? Give us feedback →",
+    labels: "feedback",
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === "separator") {
+        return <span className="cursor-default">{title}</span>;
+      }
+      return <>{title}</>;
+    },
+    defaultMenuCollapseLevel: 2,
+    toggleButton: true,
+  },
+  footer: {
+    text: (
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <div>
+          <a
+            className="flex items-center gap-1 text-current"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="AICurious Website"
+            href="https://aicurious.io"
+          >
+            <div className="pt-0 mt-0">
+              Developed by{" "}
+              <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 text-bold">
+                AI Curious
+              </span>
+              .
+            </div>
+          </a>
+        </div>
+        <p className="mt-2 text-xs">
+          © {new Date().getFullYear()} The AnyLabeling Project.
+        </p>
+      </div>
+    ),
+  },
+};
+
+export default config;
